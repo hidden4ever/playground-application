6c329e5343e7fce82addf6c7b865e3b32c275f25
Sundar <myunknownspot@gmail.com>
2017-05-28 12:04:59 +1000

    Restructuring folder

--- API/playgroundApp/urls.py
+++ API/playgroundApp/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import url,include
 from django.contrib import admin	
 
 urlpatterns = [
-    
-    url(r'^auth/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    url(r'^admin/', admin.site.urls),
+    url(r"^admin/", admin.site.urls),
+    url(r"^o/", include('oauth2_provider.urls', namespace='oauth2_provider'))
 ]
